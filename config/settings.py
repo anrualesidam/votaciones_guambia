148f080d43276b0f1e4abc7e6e6b212db9c75907
andersonruales123 <anruales.idam@gmail.com>
2025-11-09 15:22:50 -0500

    add CSRF

--- config/settings.py
+++ config/settings.py
@@ -33,6 +33,11 @@ DEBUG = True
 
 ALLOWED_HOSTS =["*","votacionesguambia-production.up.railway.app","www.votacionesguambia2025.com","votacionesguambia2025.com"] 
 
+CSRF_TRUSTED_ORIGINS = [
+    "*",
+    "https://votacionesguambia-production.up.railway.app",
+    "https://www.votacionesguambia2025.com","https://votacionesguambia2025.com"
+]
 
 # Application definition
 
